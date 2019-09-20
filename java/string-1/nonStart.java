685ec47b1b30e65caef1c84739756943b8a82f0b
satyag16 <45633767+satyag16@users.noreply.github.com>
2019-09-19 21:21:06 -0500

    Update nonStart.java

--- java/string-1/nonStart.java
+++ java/string-1/nonStart.java
@@ -2,5 +2,5 @@
  * each. The strings will be at least length 1.
  */
 public String nonStart(String a, String b) {
-    return a.substring(1) + b.substring(1);
+  return a.substring(1,a.length()) + b.substring(1,b.length());
 }
